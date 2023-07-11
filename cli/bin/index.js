a343c64860afb40394127e71b1f7fa8347f4818b
Bhargav <bhargav.eranki@gmail.com>
2023-07-11 13:14:27 -0700

    added manual branch based mapping viz - updated graphical mapping to showcase open and occupied ports

--- cli/bin/index.js
+++ cli/bin/index.js
@@ -9,6 +9,7 @@ import chalkAnimation from 'chalk-animation';
 import glob from "glob";
 import objectTreeify from "object-treeify";
 import lodash from "lodash";
+import fs from "fs";
 import { createInterface } from "readline";
 
 const yargs = _yargs(hideBin(process.argv));
@@ -105,19 +106,59 @@ if (yargs.usage(usage).argv.b != null) {
 }
 
 if (yargs.usage(usage).argv.m != null) {
-    if (yargs.usage(usage).argv.g != null) {
-        drawMockHub();
-        console.log("\n*Attempt to replicate REV Robotics Control/Expansion Hub (depending on mapping ID). NOT EXACT, but as close as you can get in a terminal...I think.");
-    } else if (yargs.usage(usage).argv.p != null) {
-        console.log('sample');
+    try {
+        const data = fs.readFileSync("pkg/util/mappings.txt", 'utf8');
+        let lines = data.split("\r\n");
+
+        let map = new Map();
+        map.set('motor', []);
+        map.set('servo', []);
+        map.set('pwm', []);
+        map.set('i2c', []);
+        map.set('digital', []);
+        map.set('analog', []);
+    
+
+        let tag = lines[0].replace("!", "");
+        //console.log(tag);
+        for (let i = 1; i < lines.length; i++) {
+            if (lines[i].length > 0) {
+                switch (lines[i]) {
+                    case '@motors':
+                        for (let j = i+1; j < i+5; j++) {
+                            let elems = lines[j].trim().split("|");
+                            if (elems.length > 1) {
+                                //console.log(elems);
+                                //console.log(lines[j].substring());
+                                let name = elems[0].substring(1).trim();
+                                let type = elems[1].trim();
+                                //console.log(name," -> ", type);
+                                map.get('motor').push(name + " -> " + type);
+                                i++;
+                                //console.log(map);
+                            }
+                        }
+                }
+            }
+        }
+
+        if (yargs.usage(usage).argv.g != null) {
+            drawMockHub(map, tag);
+            console.log("\n*Attempt to replicate REV Robotics Control/Expansion Hub (depending on mapping ID). NOT EXACT, but as close as you can get in a terminal...I think.");
+        } else if (yargs.usage(usage).argv.p != null) {
+            listMappings(map, tag);
+        }
+
+    } catch (err) {
+        console.log(err);
     }
 }
 
 
 /*
-    
+    Draws mock hub w/ mappings and all open ports. does not detail device types, only which ports they occupy. requires ANSI support for color.
 */
-function drawMockHub() {
+function drawMockHub(map, hub_id) {
     let stringToDisplay = ""
     let w = 30, h = 25;
 
@@ -132,9 +173,18 @@ function drawMockHub() {
         stringToDisplay += "\t";
 
         if (i % 4 == 0 && i >= 8 && motorPorts <= 3) {
-            stringToDisplay += "MTR.pt" + chalk.greenBright("(" + (motorPorts++) + ")\t");
+            if (map.get("motor").length > motorPorts) {
+                let a = map.get("motor")[motorPorts].split(" ")[0] + chalk.redBright("(" + (motorPorts++) + ")");
+                if (a.length <= 16) {
+                    stringToDisplay += (a + "\t\t\t");
+                } else {
+                    stringToDisplay += (a + "\t\t");
+                } 
+            } else {
+                stringToDisplay += "MTR.pt" + chalk.greenBright("(" + (motorPorts++) + ")\t\t");
+            }
         } else {
-            stringToDisplay += "\t\t";
+            stringToDisplay += "\t\t\t";
         }
         for (let j = 0; j < w; j++) {
             if (i == 0 || i == h-1 || (i == h-4 && j > 4 && j < w - 5)) {
@@ -159,7 +209,11 @@ function drawMockHub() {
                 stringToDisplay += chalk.gray("__");
             } else {
                 if (i < h - 4 && (j > 4 && j < w - 5)) {
-                    stringToDisplay += chalk.bgHex("#FFA500")("  ");
+                    if (i >= h/2 - hub_id.length/2 && i < h/2 + hub_id.length/2 && j == 5) {
+                        stringToDisplay += chalk.bgHex("#FFA500")(hub_id.charAt(i - (h/2 - hub_id.length/2))) + chalk.bgHex("#FFA500")(" ");
+                    } else {
+                        stringToDisplay += chalk.bgHex("#FFA500")("  ");
+                    }
                 } else {
                     stringToDisplay += "  ";
                 }
@@ -169,7 +223,11 @@ function drawMockHub() {
         stringToDisplay += "\t"
         
         if (i < 5 && i >= 1) {
-            stringToDisplay += chalk.greenBright("(" + (pwmPorts++) + ")") + "PWM.pt";
+            if (map.get('pwm').length > pwmPorts) {
+                stringToDisplay += chalk.redBright("(" + (pwmPorts++) + ")") + map.get('pwm')[pwmPorts-1];
+            } else {
+                stringToDisplay += chalk.greenBright("(" + (pwmPorts++) + ")") + "PWM.pt";
+            }
         }
 
         if (i >= 6 && i < 10) {
@@ -199,4 +257,23 @@ function drawMockHub() {
                         "\n\t\t\t\t\t\t   SRV.pts(0-5)";
 
     console.log(stringToDisplay);
+}
+
+function listMappings(map, hub_id) {
+    let b1 = "├─";
+    let b2 = "└─";
+    let b3 = "│";
+
+    let stringToDisplay = "";
+
+    stringToDisplay += (b1 + " " + hub_id + "\n");
+    map.forEach(function(val, key)  {
+        let title = b3 + ' '.repeat(hub_id.length/2) + b2 + " " + key;
+        stringToDisplay += title + "\n";
+        val.forEach(element => {
+           stringToDisplay += (b3 + ' '.repeat(title.length*3/4) + b2 + " " + element + "\n");
+        });
+    });
+
+    console.log(chalk.blueBright(stringToDisplay));
 }
